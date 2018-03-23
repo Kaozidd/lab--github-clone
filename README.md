90986c50845c09b494c7ebdf2f1789a12369308f
kaozidd <arthur_hemingway@outlook.com>
2018-03-22 20:28:36 -0600

    Lab update

---
+++ README.md
@@ -0,0 +1,5 @@
+# Github Clone
+
+## Week 05, Lab
+
+Only markup
---
+++ src/scripts/app.js
@@ -0,0 +1,3 @@
+import {Router} from 'director/build/director'
+import repoColumnTemplate from './repoColumn'
+import userColumnTemplate from './userColumn'
